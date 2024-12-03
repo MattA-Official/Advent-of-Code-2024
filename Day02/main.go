91c7b383ebea9133874d49271f45b8fcd0a0e4bb
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2024-12-03 00:36:13 +0000

    Day02 puzzle and input

---
+++ Day02/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Check if the input file is provided
+	if len(os.Args) < 2 {
+		fmt.Println("Missing input file")
+		return
+	}
+
+	// Open the input file
+	fileName := os.Args[1]
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Read the lines from the file
+	lines, err := readLines(file)
+	if err != nil {
+		fmt.Println("Error reading lines:", err)
+		return
+	}
+
+	// Run both tasks
+	task1(lines)
+	task2(lines)
+}
+
+func readLines(file *os.File) ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+func task1(lines []string) {
+
+}
+
+func task2(lines []string) {
+
+}
